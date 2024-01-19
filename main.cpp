8e68caf1ac4738369d725008f069d8787e90738d
MichalRogo <252821@edu.p.lodz.pl>
2024-01-19 09:13:34 +0100

    01 zmiana metody Policz

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ int Policz(int a, int b){
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "W pierwszym brachu; 2+3=" << Policz(2,3) << std::endl;
+    std::cout << "W pierwszym branchu; 2+3=" << Policz(2,3) << std::endl;
     return 0;
 }
